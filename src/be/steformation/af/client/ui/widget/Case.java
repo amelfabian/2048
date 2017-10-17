f715f8fffa4e77463485441f9c94d00b541e8d95
amelfabian <amelfabian@gmail.com>
2017-10-17 10:19:32 +0200

    table

---
+++ src/be/steformation/af/client/ui/widget/Case.java
@@ -0,0 +1,11 @@
+package be.steformation.af.client.ui.widget;
+
+import gwt.material.design.client.ui.MaterialLabel;
+
+public class Case extends MaterialLabel {
+
+		public Case() {
+			super();
+			
+		}
+}
--- src/be/steformation/af/client/ui/widget/MyTable.java
+++ src/be/steformation/af/client/ui/widget/MyTable.java
@@ -3,12 +3,25 @@ package be.steformation.af.client.ui.widget;
 
 
 
+import gwt.material.design.client.ui.MaterialColumn;
 import gwt.material.design.client.ui.MaterialPanel;
+import gwt.material.design.client.ui.MaterialRow;
 
 public class MyTable extends MaterialPanel{
-
+	
+		Case table[][] = new Case[4][4];
 		public MyTable() {
-			for(int i=0; i<4 ; i++){
+			for (int i = 0; i < table.length; i++) {
+	            MaterialRow ligne = new MaterialRow();
+	            for (int j = 0; j < table.length; j++) {
+	                MaterialColumn colonne = new MaterialColumn();
+	                Case cellule = new Case();
+	                colonne.add(cellule);
+	                ligne.add(colonne);
+	                table[i][j] = cellule;
+	            }
+	            add(ligne);
+	        }
 				
 			}
 		}
@@ -18,4 +31,4 @@ public class MyTable extends MaterialPanel{
 		
 	
 
-}
+
